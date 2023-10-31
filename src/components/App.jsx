83da3d731a79315150b96e304e18e5308d60ae6e
Alex Yuan <alexyuan0.ay@gmail.com>
2023-10-31 14:59:24 -0500

    issues with pity right now, see rollTen function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,7 +7,8 @@ import Roll from './Roll'
 function App() {
   const [rolls, setRolls] = useState([])
   const [golds, setGolds] = useState(0)
-  let pity = 0
+  const [numRolls, setNumRolls] = useState(0)
+  const [pity, setPity] = useState(0)
 
   const chance = {
     gold: 1.6,
@@ -19,20 +20,22 @@ function App() {
     const ten = []
     for(let i=0; i<10; i++){
       let p = 0
+      setNumRolls((prev) => prev+1)
+      if(pity>=90){ // hit pity
+        ten.push('gold')
+        setPity(0)
+        setGolds(golds+1)
+        continue
+      }
       for (const [rarity, prob] of Object.entries(chance)){
         p += prob
-        if(pity++===90){ // hit pity
-          ten.push('gold')
-          pity = 0
-          setGolds(golds+1)
-          break
-        }
         if (Math.random()*100 < p){
           ten.push(rarity)
           if (rarity==='gold'){
-            pity = 0
+            setPity(0)
             setGolds(golds+1)
           }
+          else setPity((prev) => prev+1)
           break
         }
       }
@@ -44,9 +47,12 @@ function App() {
     <div style={{alignItems: 'center'}}>
       <h1>Roll Simulator</h1>
       <h2>Number of 5 stars: {golds}</h2>
+      <h2>Number of rolls: {numRolls}</h2>
+      <h2>Pity: {pity}</h2>
+      <h3>Computed chance of a 5*: {numRolls ? (golds/numRolls*100).toFixed(2)+"%": "No rolls completed yet."}</h3>
       <div style={{display: 'flex', marginBottom: '1rem'}}>
-        {rolls.map((roll) => {
-          return  <Roll rarity={roll}/> 
+        {rolls.map((roll, index) => {
+          return  <Roll rarity={roll} key={index}/> 
         })}
       </div>
       <button onClick={rollTen}>Roll 10</button>
