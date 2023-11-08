82d73ffbd8a50bd1bbe3f8a8c0ddedffba7ab71a
Alex Yuan <alexyuan0.ay@gmail.com>
2023-11-07 18:44:58 -0600

    Added basic modals, next add some more functionality and later reconcile styling

--- src/components/Roll.jsx
+++ src/components/Roll.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState , useRef} from 'react'
+import { Modal, Button, ModalBody } from 'react-bootstrap'
 import './Roll.css'
 
-function Roll (props) {
-    const rarity = "roll " + props.rarity
-    return (<div className={rarity}>
 
-    </div>)
+function Roll (props) {
+    const [show, setShow] = useState(false);
+    const rarity = "roll " + props.rarity
+    const modalRef = useRef();
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    // useEffect, handleClickOutside, and modalRef here so modal keeps basic click outside properties
+    useEffect(() => {
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+          document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, []);
+    
+    const handleClickOutside = (event) => {
+        if (modalRef.current && !modalRef.current.contains(event.target)) {
+            handleClose();
+        }
+    };
+
+    return (
+        <div className={rarity} onClick={handleShow}>
+            <Modal show={show} onHide={handleClose} centered>
+                <ModalBody ref={modalRef} className={rarity} style={{height: '50vw', width: '50vw'}}>
+                </ModalBody>
+            </Modal>
+        </div>
+    )
 }
 
 export default Roll
